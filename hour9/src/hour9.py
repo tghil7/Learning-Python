53940f1126d0ac09edf78fd4a6f402f7ec7a3137
tghil7 <tghil7@aol.com>
2017-05-28 13:00:03 -0500

    Function using dictionary
    Added a function to store student grades in a dictionary

--- hour9/src/hour9.py
+++ hour9/src/hour9.py
@@ -2,5 +2,22 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+student_grade = {}
+def add_student_grade():
+    while True: 
+               name = raw_input("Please give me the name of the student (q to quit):")              
+               if name == 'q':
+                       print "Ok, printing grades!"
+                       for key, value in student_grade.iteritems():
+                              print key, value
+                       break
+               grade = raw_input("Please give me their grade:")
+               student_grade[name] = grade
+
+
+
 if __name__ == "__main__":
-    print "Hello World"
+    add_student_grade()
+    
+    
+    
